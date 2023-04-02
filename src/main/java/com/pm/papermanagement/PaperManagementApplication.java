6ef01fef575e2d0249839d914698651e7e05e72e
zyc <skyzyc@126.com>
2023-04-02 10:35:49 +0800

    create project

---
+++ src/main/java/com/pm/papermanagement/PaperManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pm.papermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperManagementApplication.class, args);
+    }
+
+}
